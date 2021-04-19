774016558fb33995f2b5e957947727c1fbb0c52b
z1011lu <zjlu@ucsc.edu>
2021-04-19 12:10:31 -0700

    Spaceships added

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -6,7 +6,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
       // add object to existing scene
       scene.add.existing(this);
 
-      this.isFireing = false;
+      this.isFiring = false;
       this.moveSpeed = 2;
     }
 
@@ -15,7 +15,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         if(!this.isFiring){
             if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
                 this.x -= this.moveSpeed;
-            }else if (keyRIGHT.isDown && this.x <= game.cconfig.width - borderUUISize - this.width){
+            }else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
                 this.x += this.moveSpeed;
             }
         }
@@ -35,7 +35,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.isFiring = false;
             this.y = game.config.height - borderUISize - borderPadding;
         }
-        this.p1Rocket.update();
+        
     }
 
 }
