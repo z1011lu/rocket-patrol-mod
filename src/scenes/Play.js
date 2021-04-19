404bcf8fcacf8d949b522121bf93cc24ff264067
z1011lu <zjlu@ucsc.edu>
2021-04-18 20:23:01 -0700

    Greenbar and white border

---
+++ src/scenes/Play.js
@@ -0,0 +1,18 @@
+class Play extends Phaser.Scene {
+    constructor(){
+        super("playScene");
+    }
+
+
+    create() {
+        //this.add.text(20, 20, "Rocket Patrol Play");
+        // green UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        // white borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+
+    }
+}
